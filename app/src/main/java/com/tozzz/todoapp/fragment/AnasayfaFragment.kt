16140e5b214be30995b4210fe2a05caafe7dc5b9
Merve Töz <47971059+mervetozzz@users.noreply.github.com>
2022-05-12 23:34:55 +0300

    mvvmUpdate

--- app/src/main/java/com/tozzz/todoapp/fragment/AnasayfaFragment.kt
+++ app/src/main/java/com/tozzz/todoapp/fragment/AnasayfaFragment.kt
@@ -3,11 +3,14 @@ package com.tozzz.todoapp.fragment
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tozzz.todoapp.R
+import com.tozzz.todoapp.adapter.YapilacaklarAdapter
 import com.tozzz.todoapp.databinding.FragmentAnasayfaBinding
 import com.tozzz.todoapp.entity.Yapilacaklar
 
@@ -18,15 +21,24 @@ class AnasayfaFragment : Fragment(),SearchView.OnQueryTextListener {
         tasarim.toolbarAnasayfa.title = "Yapılacaklar Listesi"
         (activity as AppCompatActivity).setSupportActionBar(tasarim.toolbarAnasayfa)
 
+        tasarim.rv.layoutManager = LinearLayoutManager(requireContext())
+
+        val yapilacaklarListesi = ArrayList<Yapilacaklar>()
+        val i1 = Yapilacaklar(1,"Ekmek Al")
+        val i2 = Yapilacaklar(2,"kahvaltı")
+        val i3 = Yapilacaklar(3,"spor")
+        val i4 = Yapilacaklar(4,"Kotlin")
+
+        yapilacaklarListesi.add(i1)
+        yapilacaklarListesi.add(i2)
+        yapilacaklarListesi.add(i3)
+        yapilacaklarListesi.add(i4)
+
+        val adapter = YapilacaklarAdapter(requireContext(),yapilacaklarListesi)
+        tasarim.rv.adapter = adapter
 
         tasarim.fabEkle.setOnClickListener{
             Navigation.findNavController(it).navigate(R.id.kayıtGecis)
-        }
-
-        tasarim.buttonDetay.setOnClickListener{
-            val yapilacaklar = Yapilacaklar(1,"Ekmek Al")
-            val gecis = AnasayfaFragmentDirections.detayGecis(yapilacak = yapilacaklar)
-            Navigation.findNavController(it).navigate(gecis)
         }
         return tasarim.root
     }
